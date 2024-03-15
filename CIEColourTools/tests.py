18b1e26d754e5fb051848d13a53ac5ba21dfe6dd
Marcus <marcus.d.lowe@gmail.com>
2024-03-15 13:37:40 +0000

    added more funcs

--- CIEColourTools/tests.py
+++ CIEColourTools/tests.py
@@ -1,11 +1,22 @@
 import ColourTools as CT
 import numpy as np
+import matplotlib.pyplot as plt
 
 #spectra = CT.ASCIIXYtoArr("CIEColourTools/alizarinXY.txt")
-spectra = np.loadtxt("CIEColourTools/alazarinEXP.csv",delimiter=',')
-spectra[:,1] = 10**(spectra[:,1] - 4)
-X,Y,Z = CT.spectraToXYZ(spectra,.5)
+#spectra = np.loadtxt("CIEColourTools/alazarinEXP.csv",delimiter=',')
+#spectra[:,1] = 10**(spectra[:,1] - 4)
+spectra = CT.spectrumToArr("CIEColourTools/AQ-A_pbe0-631++g2d2p_esd-ahas.spectrum",1e-5)
+#print(spectra)
+
+#plt.plot(spectra[:,0],spectra[:,1])
+#plt.show()
+
+X,Y,Z = CT.spectraToXYZ(spectra,50)
+L,a,b = CT.XYZtoLab(X,Y,Z)
+C,h = CT.abtoCh(a,b)
 print(X,Y,Z)
+print(L,a,b)
+print(C,h)
 """
 import matplotlib.pyplot as plt
 plt.plot(spectra[:,0],x_b)
