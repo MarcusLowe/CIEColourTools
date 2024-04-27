042de0a5cbb9dc627d5ae6f4e466eb09746d864d
Grongle <marcus.d.lowe@gmail.com>
2024-04-27 02:14:12 +0100

    added comments

--- CIEColourTools/ColourTools.py
+++ CIEColourTools/ColourTools.py
@@ -1,11 +1,35 @@
 import numpy as np
 
 def ASCIIXYtoArr(filename,scale):
+    """
+    Takes in ASCII xy file generated from GabEdit and converts into a numpy array
+    -
+    INPUT
+    filename: string 
+        Filepath of ASCII xy file
+    scale: float
+        Scaling factor for outputted spectra
+
+    RETURNS\n
+    Numpy array of wavelengths, Numpy array of corresponding intensities
+    """
     f = np.loadtxt(filename)
     f[:,1] *= scale
     return f[:,0], f[:,1]
 
 def spectrumToArr(filename,scale):
+    """
+    Takes in .spectrum file generated from Orca's ESD method and converts into a numpy array
+    -
+    INPUT
+    filename: string 
+        Filepath of .spectrum file
+    scale: float
+        Scaling factor for outputted spectra
+
+    RETURNS\n
+    Numpy array of wavelengths, Numpy array of corresponding intensities
+    """
     f = np.loadtxt(filename)[:,0:2]
     #f[:,0] = 10_000_000/f[:,0]
     f = f[f[:,0] < 850]
@@ -15,23 +39,43 @@ def spectrumToArr(filename,scale):
     return f[:,0], f[:,1]
 
 def x_bar(wavelengths):
+    """
+    Returns interpolated value of the CIE XYZ 1931 xbar standard observer function at the inputted wavelengths
+    -
+    """
     colorFuncs = np.loadtxt("CIEColourTools/CIE_xyz_1931_2deg.csv",delimiter=',')
     return np.interp(wavelengths,colorFuncs[:,0],colorFuncs[:,1])
 
 def y_bar(wavelengths):
+    """
+    Returns interpolated value of the CIE XYZ 1931 ybar standard observer function at the inputted wavelengths
+    -
+    """
     colorFuncs = np.loadtxt("CIEColourTools/CIE_xyz_1931_2deg.csv",delimiter=',')
     return np.interp(wavelengths,colorFuncs[:,0],colorFuncs[:,2])
 
 def z_bar(wavelengths):
+    """
+    Returns interpolated value of the CIE XYZ 1931 zbar standard observer function at the inputted wavelengths
+    -
+    """
     colorFuncs = np.loadtxt("CIEColourTools/CIE_xyz_1931_2deg.csv",delimiter=',')
     return np.interp(wavelengths,colorFuncs[:,0],colorFuncs[:,3])
 
 def D65_ill(wavelengths):
+    """
+    Returns interpolated value of the CIE standard illuminant D65 spectrum function at the inputted wavelengths
+    -
+    """
     D65 = np.loadtxt("CIEColourTools/CIE_std_illum_D65.csv",delimiter=',')
     #D65 = D65[360 <= D65[:,0]]
     return np.interp(wavelengths,D65[:,0],D65[:,1])
 
 def spectraToXYZ(spectraWave,spectraInt,K,shift=0):
+    """
+    Converts an inputted UV-vis absorbance spectra into the 1931 CIE XYZ colour coordinates
+    -
+    """
     
     #wavelengths = spectra[:,0][np.where((360 <= spectra[:,0]) & (spectra[:,0] <= 830))]
     #spectrum = spectra[:,1][np.where((360 <= spectra[:,0]) & (spectra[:,0] <= 830))]
@@ -66,13 +110,24 @@ def spectraToXYZ(spectraWave,spectraInt,K,shift=0):
     X = K*np.trapz(spectrum*x_b*I,wavelengths)/N
     Y = K*np.trapz(spectrum*y_b*I,wavelengths)/N
     Z = K*np.trapz(spectrum*z_b*I,wavelengths)/N
-    #print([X*N,Y*N,Z*N] )
+
+    if X > .950498:
+        print("X outside range")
+    if Y > 1:
+        print("Y outside range")
+    if Z > 1.088840:
+        print("Z outside range")
+    
     return X,Y,Z
 
 def XYZtoLab(X,Y,Z):
-    Xn = 95.0498
-    Yn = 100
-    Zn = 108.8840
+    """
+    Converts CIE 1931 XYZ coordinates into CIE L*a*b* coordinates
+    -
+    """
+    Xn = .950498
+    Yn = 1.00
+    Zn = 1.088840
     epsilon = 0.008856
     k = 903.3
 
@@ -101,13 +156,23 @@ def XYZtoLab(X,Y,Z):
 
     return L,a,b
 
-def abtoCh(a,b):
+def abtoCh(a,b,units="rad"):
+    """
+    Converts a* and b* coordinates from the CIELab space to chroma (C*) and hue (h) coordinates
+    -
+    """
     C = np.sqrt(a**2 + b**2)
     h = np.arctan(b/a)
+    if units != "rad":
+        h = 180*h/(np.pi)
 
     return C,h
 
 def XYZtosRGB(X,Y,Z):
+    """
+    Converts CIE XYZ coordinates to sRGB coordinates using standard illuminant D65 as illuminant
+    -
+    """
     XYZ = np.array([X,Y,Z])
     M = np.array([[3.2404542, -1.5371385, -0.4985314],
                   [-0.9692660, 1.8760108,  0.0415560],
@@ -123,12 +188,21 @@ def XYZtosRGB(X,Y,Z):
 
     #print(rgb**(1/2.2))
     #print(RGB)
+    
+    if np.any(RGB < 0):
+        #Not in sRGB gamut, approximating by desaturating
+        RGB -= np.min(RGB)        
+    
     if np.any(RGB > 1): #normalise
         RGB /= np.max(RGB)
     
     return RGB
 
 def XYZtoRGB(X,Y,Z):
+    """
+    Converts CIE XYZ coordinates to 1998 Adobe RGB coordinates using standard illuminant D65 as illuminant.
+    -
+    """
     xyz2rgb = np.array([[ 2.0413690, -0.5649464, -0.3446944],
                         [-0.9692660,  1.8760108,  0.0415560],
                         [ 0.0134474, -0.1183897,  1.0154096]])
@@ -141,7 +215,9 @@ def XYZtoRGB(X,Y,Z):
     return RGB
 
 def rgb_to_hex(RGB):
-        """Convert from fractional rgb values to HTML-style hex string."""
+        """
+        Convert from fractional rgb values to HTML-style hex string.
+        """
         
         hex_rgb = (255 * RGB).astype(int)
         return '#{:02x}{:02x}{:02x}'.format(*hex_rgb)
--- CIEColourTools/tests.py
+++ CIEColourTools/tests.py
@@ -3,44 +3,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#spectra = CT.ASCIIXYtoArr("CIEColourTools/Spectra/1AAQ L30 Shift 0.txt",10)
-#spectra = np.loadtxt("CIEColourTools/alazarinEXP.csv",delimiter=',')
-#specW, specI = CT.spectrumToArr("CIEColourTools/Spectra/AQ-A_pbe0-631++g2d2p_esd-ahas2.spectrum",1e-5)
-#spectra[:,1] = 10**(spectra[:,1] - 4)
-#spectra = CT.spectrumToArr("CIEColourTools/AQ-A_pbe0-631++g2d2p_esd-ahas.spectrum",5e-5)
-#print(spectra)
+if __name__ == '__main__':
 
-expSpec = pd.read_csv("CIEColourTools/Spectra/AQA_Exp_Graph.csv")
+    #spectra = CT.ASCIIXYtoArr("CIEColourTools/Spectra/1AAQ L30 Shift 0.txt",10)
+    #spectra = np.loadtxt("CIEColourTools/alazarinEXP.csv",delimiter=',')
+    #specW, specI = CT.spectrumToArr("CIEColourTools/Spectra/AQ-A_pbe0-631++g2d2p_esd-ahas2.spectrum",1e-5)
+    #spectra[:,1] = 10**(spectra[:,1] - 4)
+    #spectra = CT.spectrumToArr("CIEColourTools/AQ-A_pbe0-631++g2d2p_esd-ahas.spectrum",5e-5)
+    #print(spectra)
 
-#plt.plot(spectra[:,0],spectra[:,1])
-#plt.show()
-specW = np.array(expSpec['x'])
-specI = np.array(expSpec['y'])*0.6
-#print(specW,specI)
-X,Y,Z = CT.spectraToXYZ(specW,specI,1,shift=0)
-L,a,b = CT.XYZtoLab(X,Y,Z)
-C,h = CT.abtoCh(a,b)
-sRGB = CT.XYZtosRGB(X,Y,Z)
-shex = CT.rgb_to_hex(sRGB)
-RGB = CT.XYZtoRGB(X,Y,Z)
-hex = CT.rgb_to_hex(RGB)
-#spectra = CT.ASCIIXYtoArr("CIEColourTools/Alizarin_LHW17_Shift25.txt")
-#spectra = CT.ASCIIXYtoArr("CIEColourTools/alizarin_shifted_big.txt")
-#spectra[:,1] *= 10
-#spectra = np.loadtxt("CIEColourTools/alazarinEXP.csv",delimiter=',')
-#spectra[:,1] = 10**(spectra[:,1] - 4)
-print("XYZ ",X,Y,Z)
-print("LAB ",L,a,b)
-print("Ch ",C,h)
-print("sRGB ",sRGB)
-print("shex ",shex)
-print("RGB ",RGB)
-print("hex ",hex)
-"""
-import matplotlib.pyplot as plt
-plt.plot(spectra[:,0],x_b)
-import numpy as np
-colorFuncs = np.loadtxt("CIEColourTools/CIE_xyz_1931_2deg.csv",delimiter=',')
-plt.plot(colorFuncs[:,0],colorFuncs[:,1])
-plt.show()
-"""
+    expSpec = pd.read_csv("CIEColourTools/Spectra/AQC_Exp_Graph.csv")
+
+    #plt.plot(spectra[:,0],spectra[:,1])
+    #plt.show()
+    specW = np.array(expSpec['x'])
+    specI = np.array(expSpec['y'])*1.5
+    #print(specW,specI)
+    X,Y,Z = CT.spectraToXYZ(specW,specI,1,shift=0)
+    L,a,b = CT.XYZtoLab(X,Y,Z)
+    C,h = CT.abtoCh(a,b)
+    sRGB = CT.XYZtosRGB(X,Y,Z)
+    shex = CT.rgb_to_hex(sRGB)
+    RGB = CT.XYZtoRGB(X,Y,Z)
+    hex = CT.rgb_to_hex(RGB)
+    #spectra = CT.ASCIIXYtoArr("CIEColourTools/Alizarin_LHW17_Shift25.txt")
+    #spectra = CT.ASCIIXYtoArr("CIEColourTools/alizarin_shifted_big.txt")
+    #spectra[:,1] *= 10
+    #spectra = np.loadtxt("CIEColourTools/alazarinEXP.csv",delimiter=',')
+    #spectra[:,1] = 10**(spectra[:,1] - 4)
+    print("XYZ ",X,Y,Z)
+    print("LAB ",L,a,b)
+    print("Ch ",C,h)
+    print("sRGB ",sRGB)
+    print("shex ",shex)
+    print("RGB ",RGB)
+    print("hex ",hex)
+    """
+    import matplotlib.pyplot as plt
+    plt.plot(spectra[:,0],x_b)
+    import numpy as np
+    colorFuncs = np.loadtxt("CIEColourTools/CIE_xyz_1931_2deg.csv",delimiter=',')
+    plt.plot(colorFuncs[:,0],colorFuncs[:,1])
+    plt.show()
+    """
